bb4081ee500c95b51eecd58513c85147c1e00329
Graciela Deras <graciela.deras1982@gmail.com>
2022-03-21 23:48:50 -0600

    Agregando tarea #8

---
+++ EJERCICIOS/Tarea #8/tarea#8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+float raizCuadrada ( float numero )
+{
+    return sqrt (numero); 
+}
+
+int main(int argc, char const *argv[])
+
+{
+    float n1 = 0;                                                                 
+    
+    cout << " \n CALCULO PARA ENCONTRAR LA RAIZ CUADRADA DE UN NUMERO " <<endl;
+    cout << " \n Digite el valor del numero que desea calcular : ";
+    cin  >> n1; 
+    
+    cout << " \n La raiz cuadrada de " << n1 << " es: ";             
+    cout << raizCuadrada (n1);
+    cout << endl;
+
+    return 0;
+}
+
