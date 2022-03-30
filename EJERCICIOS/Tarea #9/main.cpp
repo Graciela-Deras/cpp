87e67761778e0ee2735cfd2ea96c98c6ffe093f3
Graciela Deras <graciela.deras1982@gmail.com>
2022-03-29 23:49:31 -0600

    Agregando ejercicio #9

--- EJERCICIOS/Tarea #9/main.cpp
+++ EJERCICIOS/Tarea #9/main.cpp
@@ -39,6 +39,7 @@ int main(int argc, char const *argv[])
         else 
         {
             cout << "\n Perdiste, intentalo de nuevo :(" << endl;
+            system ("pause");
         }
 
         cout << endl;
