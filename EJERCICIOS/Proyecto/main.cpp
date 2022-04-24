d5219d7af09eadf9d7ec0539ccf9335967553739
Graciela Deras <graciela.deras1982@gmail.com>
2022-04-23 23:57:38 -0600

    modificando proyecto

--- EJERCICIOS/Proyecto/main.cpp
+++ EJERCICIOS/Proyecto/main.cpp
@@ -4,36 +4,40 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int persona = 0, salir = 1;
-    string nombre;
+    int persona = 0, cantidad = 0;
     float ninos = 0, jovenes = 0, adultos = 0, terceraEdad = 0, totalPersonas = 0;
-    float porcentajeNinos = 0, porcentajeJovenes = 0, porcentajeAdultos = 0, porcentajeTerceraEdad = 0;
+    float porcentajeNinos = 0, porcentajeJovenes = 0, porcentajeAdultos = 0, porcentajeTerceraEdad = 0;  
 
-    string nombres [persona];
-    int edad[10];
-    string datos[1][2]=
-    {
-        { " NOMBRES ", " EDADES "}
-    };
-
-    while ( salir == 1)
-    {
-
-	persona++;   
     system ("cls");
 	cout << "********************************************************" << endl;                                                                 
 	cout << "CENSO DE EDADES DE LA POBLACION EN LA ZONA DE VILLANUEVA" << endl;
 	cout << "********************************************************" << endl; 
-    cout << "Ingrese los datos solicitados acontinuacion: " << endl;
-    cout << "Persona No. " << persona << endl;
+    cout << endl;
 
-    for (int i = 0; i < 1; i++)
+    cout << "Digite la cantidad de personas a censar: ";
+    cin >> cantidad;
+    cout << endl;
+    
+    string datos[1][3]=
     {
+        { " NOMBRES ","  ||  ", " EDADES "}
+    };
+
+    string nombres [cantidad];
+    int edad[cantidad];
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        persona++; 
+        system ("cls");
+        cout << "Persona No. " << persona << endl;
+        cout << "Ingrese los datos solicitados a continuacion: " << endl;
         cout << "Nombre: ";
         cin >> nombres [i]; 
 
         cout << "Edad: ";
         cin >> edad [i];
+        cout << endl;
             
         if ( edad [i] <= 12 )
 			ninos++;                                                                    
@@ -46,49 +50,45 @@ int main(int argc, char const *argv[])
 					
 					if ( edad [i] >= 60 )
 						terceraEdad++;  
-    
     }
     
-    cout << endl;
-    cout << "Desea continuar el censo con otra persona?" << endl;
-    cout << "Presione 1 para continuar, de lo contrario ingrese el numero 0: ";
-    cin >> salir;
-    cout << endl;
-
-    }
-    
-    system("cls");
+    system ("cls");
     cout << "TABLA DE DATOS OBTENIDOS" << endl;
     cout << "========================" << endl;
-    
+
     for (int a = 0; a < 1; a++)
     {
-        for (int b = 0; b < 2; b++)
+        for (int b = 0; b < 3; b++)
         {
             cout << datos [a][b] << " "; 
         }
-        cout << endl;
     }
-    
-    for (int i = 0; i < persona; i++ )
+
+    cout << endl;
+
+    for (int a = 0; a < cantidad ; a++ )
     {
-        cout <<nombres[i]<<"  "<<edad[i]<<endl;
-        system ("pause");
-    }
-
+        cout <<nombres[a] << "      " << edad [a] << endl;
         
+    }
+    system("pause"); 
+     
 	totalPersonas = (ninos + adultos + jovenes + terceraEdad);     
-
 	porcentajeNinos = ninos / totalPersonas * 100;
 	porcentajeJovenes = jovenes / totalPersonas * 100;
 	porcentajeAdultos = adultos / totalPersonas * 100;
 	porcentajeTerceraEdad = terceraEdad / totalPersonas * 100;
     
-	cout << " RESUMEN DE DATOS RESULTANTES DEL CENSO ";
-    cout << " ************************* ";
-	cout << endl;
-    cout << " \n El total de personas encuestadas  es: " << totalPersonas;
-	cout << " \n El porcentaje de ninos en la zona es: " << porcentajeNinos << " % " << endl;
-	cout << " \n El porcentaje de personas jovenes en la zona es " << porcentajeJovenes << " % " << endl;
-	cout << " \n El porcentaje de personas adultos en la zona es del: " << porcentajeAdultos << " % " << endl;
-	cout << " \n El porcentaje de personas de la tercera en la zona es: " << porcentajeTerceraEdad << " % " << endl;
+    system ("cls");
+    cout << " *************************************** " << endl;
+	cout << " RESUMEN DE DATOS RESULTANTES DEL CENSO " << endl;
+    cout << " *************************************** " << endl;
+    cout << " El total de personas encuestadas fue: " << totalPersonas << endl;
+	cout << " Prcentaje de ninos en la zona: " << porcentajeNinos << " % " << endl;
+	cout << " Porcentaje de personas jovenes en la zona: " << porcentajeJovenes << " % " << endl;
+	cout << " Porcentaje de personas adultos en la zona: " << porcentajeAdultos << " % " << endl;
+	cout << " Porcentaje de personas de la tercera en la zona: " << porcentajeTerceraEdad << " % " << endl;
+    cout << endl;
+
+    return 0;
+}
